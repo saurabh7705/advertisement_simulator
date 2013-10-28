e07009be0981f1e43d37d2e8781d79d11b112315
Saurabh Maheshwari <saurabh@octathorpeweb.com>
2013-10-28 19:52:34 +0530

    fix

--- protected/views/advertisementUnit/index.php
+++ protected/views/advertisementUnit/index.php
@@ -1,5 +1,17 @@
 <div class="padit">
 	<center><h1>Ad Units<h1></center><br />
+	<?php $types = AdvertisementType::model()->findAll(); ?>
+
+        <div class="team_tab">
+                <ul class="nav nav-tabs m20">
+                        <?php foreach($types as $type) { ?>
+                                <li class="<?php echo ($advertisement_type_id == $type->id) ? 'active' : ''; ?>">
+                                        <?php echo CHtml::link($type->name, array('/admin/advertisementUnit/index', 'advertisement_type_id'=>$type->id)); ?>
+                                </li>
+                        <?php } ?>
+                </ul>
+        </div>
+        
 	<?php
 		$this->widget('zii.widgets.grid.CGridView', array(
 			'id'=>'team_grid',
@@ -14,11 +26,22 @@
 				),
 				'cost',
 				'impressions',
-				'description',
-				array(
-					'name'=>'advertisement_type_id',
-					'value'=>'$data->advertisement_type->name'
-				),
+				'index',
+	                        array(
+	                                'name'=>'high_frequency',
+	                                'value'=>'$data->high_frequency == 1 ? "Yes" : "No"',
+	                                'filter' => CHtml::dropDownList(
+	                                        'AdvertisementUnit[high_frequency]',
+	                                        $model->high_frequency,
+	                                        array('0' => 'No', '1' => 'Yes'),
+	                                        array('prompt' => 'All')
+	                                ),
+	                        ),
+	                        //'description',
+	                        array(
+	                                'name'=>'advertisement_type_name',
+	                                'value'=>'$data->advertisement_type->name'
+	                        ),
 				array(
 					'header'=>'Infinite/Auction',
 					'value'=>'($data->in_auction == 1) ? "Auction" : "Infinite"',
