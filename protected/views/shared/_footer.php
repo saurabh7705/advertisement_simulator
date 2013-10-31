6179b2f6ec2f58f3cf8b9a0b42c2a87cf5e32489
rajat_imac <rajat@octathorpeweb.com>
2013-10-31 15:10:47 +0530

    footer fix

--- protected/views/shared/_footer.php
+++ protected/views/shared/_footer.php
@@ -1,11 +1,11 @@
 <div class="container">
 	<footer class="row">
 		<div class="span9">
-		<img src="<?php echo Yii::app()->baseUrl; ?>/images/sponsors/SBB.png" />
+		<img style="width:330px" src="<?php echo Yii::app()->baseUrl; ?>/images/sponsors/SBB.png" />
 		<img src="<?php echo Yii::app()->baseUrl; ?>/images/sponsors/social_samosa_white.png" />
 		<img src="<?php echo Yii::app()->baseUrl; ?>/images/sponsors/Desidime.png" />
 		<img src="<?php echo Yii::app()->baseUrl; ?>/images/sponsors/lawrence.jpg" />
-		<img src="<?php echo Yii::app()->baseUrl; ?>/images/sponsors/ebayindia-with-TM.png" />		
+		<img style="width: 230px;" src="<?php echo Yii::app()->baseUrl; ?>/images/sponsors/ebayindia-with-TM.png" />		
 		<img src="<?php echo Yii::app()->baseUrl; ?>/images/sponsors/knowfest.png" />
 		<img src="<?php echo Yii::app()->baseUrl; ?>/images/sponsors/adverb.png" />
 		</div>
