f823a2b4fb476726e8f09c2eae91c28dcdaa02b6
rajat_imac <rajat@octathorpeweb.com>
2013-10-31 14:48:06 +0530

    footer images fix

--- protected/views/shared/_footer.php
+++ protected/views/shared/_footer.php
@@ -1,5 +1,6 @@
 <div class="container">
 	<footer class="row">
+		<div class="span9">
 		<img src="<?php echo Yii::app()->baseUrl; ?>/images/sponsors/SBB.png" />
 		<img src="<?php echo Yii::app()->baseUrl; ?>/images/sponsors/social_samosa_white.png" />
 		<img src="<?php echo Yii::app()->baseUrl; ?>/images/sponsors/Desidime.png" />
@@ -8,5 +9,10 @@
 		<img src="<?php echo Yii::app()->baseUrl; ?>/images/sponsors/Hitwicket.png" />
 		<img src="<?php echo Yii::app()->baseUrl; ?>/images/sponsors/knowfest.png" />
 		<img src="<?php echo Yii::app()->baseUrl; ?>/images/sponsors/adverb.png" />
+		</div>
+		<div class="span3">
+			<h3 class="powered_by">Powered By</h3>
+		</div>
+		<br clear="all" />
 	</footer>
 </div>
