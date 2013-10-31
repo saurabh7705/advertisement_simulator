b55b3521ab3faa6499d2a1b236df02d324cc0307
rajat_imac <rajat@octathorpeweb.com>
2013-10-31 15:05:24 +0530

    footer fix

--- protected/views/shared/_footer.php
+++ protected/views/shared/_footer.php
@@ -5,13 +5,13 @@
 		<img src="<?php echo Yii::app()->baseUrl; ?>/images/sponsors/social_samosa_white.png" />
 		<img src="<?php echo Yii::app()->baseUrl; ?>/images/sponsors/Desidime.png" />
 		<img src="<?php echo Yii::app()->baseUrl; ?>/images/sponsors/lawrence.jpg" />
-		<img src="<?php echo Yii::app()->baseUrl; ?>/images/sponsors/ebayindia-with-TM.png" />
-		<img src="<?php echo Yii::app()->baseUrl; ?>/images/sponsors/Hitwicket.png" />
+		<img src="<?php echo Yii::app()->baseUrl; ?>/images/sponsors/ebayindia-with-TM.png" />		
 		<img src="<?php echo Yii::app()->baseUrl; ?>/images/sponsors/knowfest.png" />
 		<img src="<?php echo Yii::app()->baseUrl; ?>/images/sponsors/adverb.png" />
 		</div>
 		<div class="span3">
 			<h3 class="powered_by">Powered By</h3>
+			<a href="http://hitwicket.com?utm_source=adverb_footer&utm_medium=adverb_site&utm_campaign=adverb" target="_blank"><img src="<?php echo Yii::app()->baseUrl; ?>/images/sponsors/Hitwicket.png" /></a>
 		</div>
 		<br clear="all" />
 	</footer>
